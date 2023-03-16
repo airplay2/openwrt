5223ff92ca7f383298e0939d64173e366b318743
airplay2 <53650968+airplay2@users.noreply.github.com>
2023-03-16 12:16:12 +0800

    Update m3u8mp3+.sh

--- m3u8mp3+.sh
+++ m3u8mp3+.sh
@@ -2,9 +2,9 @@
  while true;do
 ffmpeg=`ps | grep -E "ffmpeg" | awk -F ' ' {'print $5'} | awk "NR==1"`
 if [ "$ffmpeg" = "ffmpeg" ]; then
-sleep 4
 echo Streaming succeeded
 else
 ffmpeg -re -i "http://ngcdn001.cnr.cn/live/zgzs/index.m3u8" -ar 44100 -threads 1 -ac 1 -f mp3 "icecast://source:hackme@192.168.31.1:8001/zgzs1"
 fi
+sleep 3
 done

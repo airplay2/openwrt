d8f662ac9858153d5427ccc2ab70381f4711953a
airplay2 <53650968+airplay2@users.noreply.github.com>
2023-03-16 09:39:59 +0800

    Update m3u8mp3+.sh

--- m3u8mp3+.sh
+++ m3u8mp3+.sh
@@ -6,7 +6,7 @@ FFMPEG(){
 stop=0
 while [ "$stop" = "0" ];do
 echo 这个地方是守护进程的命令
-sh /root/m3u8mp3.sh
+ffmpeg -re -i "http://ngcdn001.cnr.cn/live/zgzs/index.m3u8" -ar 44100 -threads 1 -ac 1 -f mp3 "icecast://source:hackme@192.168.31.1:8001/zgzs1"
 stop=1
 n=$(($n+1))
 n=1
